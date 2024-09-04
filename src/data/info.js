58126360f4f3ca6d48cc8597b7d6855b9990552f
Hidan12 <marcarfe12@gmail.com>
2024-09-04 09:55:44 -0500

    creation of the page

---
+++ src/data/info.js
@@ -0,0 +1,43 @@
+export default {
+    home: "Harry James Potter is a half-blood wizard and the only son of James and Lily Potter. He is the only known person to have survived the curse of Avada Kedavra, doing so on two occasions. Both times the curse was cast by Lord Voldemort.",
+    descriptionHouse: {
+        house: {
+            description: "Students belonging to each house do not necessarily have all the qualities that characterize each one of them. There are cases in which the Sorting Hat receives students who make it doubt which house they should be assigned to. If the Hat hesitates for more than five minutes, that student is called a Hatstall."
+        },
+        Gryffindor: {
+            description: "Gryffindor House was founded by the famous wizard Godric Gryffindor. Godric only accepted witches and wizards who had bravery, goodwill, courage and chivalry into his house, as these are the qualities of a true Gryffindor. The house colours are gold and scarlet and its symbol is a lion. The most prized relic of the house is the sword of Godric Gryffindor, belonging, as its name suggests, to the founder of the house. Students of this house spend most of their time in Gryffindor Tower, located on the seventh floor of Hogwarts Castle."
+        },
+        Hufflepuff: {
+            description: "The Hufflepuff Common Room is located in a cellar in the same underground passage as the kitchen. Initially, Hufflepuff sought students who simply wanted to belong to that house, although nowadays it seeks loyal, honest students who are not afraid of hard work. The founder is none other than the witch Helga Hufflepuff, a childhood friend of Rowena Ravenclaw. Helga was a very noble, friendly witch and the main driving force behind Hogwarts accepting Muggle-born students. The main relic of the house is Helga Hufflepuff's cup. The symbol of the house is a black badger and its representative colors are yellow and charcoal black."
+        },
+        Ravenclaw:{
+            description: "Ravenclaw House is located in a tower in the west wing of the castle. At the entrance is a statue shaped like an eagle that tells riddles and only lets you in if you solve them. Its colours are blue and bronze. Ravenclaw seeks creative, curious students who always look for the answer. It was founded by the witch, born in the glens of Scotland, Rowena Ravenclaw. She is supposedly the main inventor of the name, location and format of Hogwarts. She is also the cause of the stairs moving. Her main relic is Rowena Ravenclaw's diadem. The symbol of the house is the eagle, although in some versions of the crest it is a raven."
+        },
+        Slytherin:{
+            description: `Slytherin House is characterized primarily by ambition and cunning. It was founded by the wizard Salazar Slytherin. The Common Room is located in the dungeons, passing through a series of numerous underground passages. Possibly it is reached through the Hogwarts Hall. Specifically it is located under the Black Lake, making the common room cold and with a greenish hue, since there are windows that look out onto the waters. It is accessed through a highly concealed door in a stone wall, saying a password required. The only known one is "Pureblood". Its main relic is Salazar Slytherin's locket. The representative animal is the snake, its colors are green and silver and the element is water, associated with cunning and coldness.`
+        }
+    }
+    
+}
+
+
+
+
+// [
+//     {
+//         id:"Gryffindor",
+//         description: "Gryffindor House was founded by the famous wizard Godric Gryffindor. Godric only accepted witches and wizards who had bravery, goodwill, courage and chivalry into his house, as these are the qualities of a true Gryffindor. The house colours are gold and scarlet and its symbol is a lion. The most prized relic of the house is the sword of Godric Gryffindor, belonging, as its name suggests, to the founder of the house. Students of this house spend most of their time in Gryffindor Tower, located on the seventh floor of Hogwarts Castle."
+//     },
+//     {
+//         id:"Hufflepuff", 
+//         description: "The Hufflepuff Common Room is located in a cellar in the same underground passage as the kitchen. Initially, Hufflepuff sought students who simply wanted to belong to that house, although nowadays it seeks loyal, honest students who are not afraid of hard work. The founder is none other than the witch Helga Hufflepuff, a childhood friend of Rowena Ravenclaw. Helga was a very noble, friendly witch and the main driving force behind Hogwarts accepting Muggle-born students. The main relic of the house is Helga Hufflepuff's cup. The symbol of the house is a black badger and its representative colors are yellow and charcoal black."
+//     },
+//     {
+//         id:"Ravenclaw",
+//         description: "Ravenclaw House is located in a tower in the west wing of the castle. At the entrance is a statue shaped like an eagle that tells riddles and only lets you in if you solve them. Its colours are blue and bronze. Ravenclaw seeks creative, curious students who always look for the answer. It was founded by the witch, born in the glens of Scotland, Rowena Ravenclaw. She is supposedly the main inventor of the name, location and format of Hogwarts. She is also the cause of the stairs moving. Her main relic is Rowena Ravenclaw's diadem. The symbol of the house is the eagle, although in some versions of the crest it is a raven."
+//     },
+//     {
+//         id: "Slytherin",
+//         description: `Slytherin House is characterized primarily by ambition and cunning. It was founded by the wizard Salazar Slytherin. The Common Room is located in the dungeons, passing through a series of numerous underground passages. Possibly it is reached through the Hogwarts Hall. Specifically it is located under the Black Lake, making the common room cold and with a greenish hue, since there are windows that look out onto the waters. It is accessed through a highly concealed door in a stone wall, saying a password required. The only known one is "Pureblood". Its main relic is Salazar Slytherin's locket. The representative animal is the snake, its colors are green and silver and the element is water, associated with cunning and coldness.`
+//     }
+// ]
---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+      return {
+        message: "funciona"
+      }
+    },
+    created(){
+        console.log(this.message);
+    },
+    methods: {
+        
+    },
+    computed:{
+
+    }
+
+  }).mount('#app')
