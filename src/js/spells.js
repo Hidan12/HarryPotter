16e606d89dbf8b483e05d6b432eb79fbd46e9351
Danip25 <danielsambonic@hotmail.com>
2024-09-07 09:15:08 -0500

    spells page created

--- src/js/spells.js
+++ src/js/spells.js
@@ -1,19 +1,45 @@
+let urlApi= "https://hp-api.onrender.com/api/spells"
+const img = {
+  aberto:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv3VHGiPtsg5pKOeC9e38e12xUslxe2us5bw&s"
+}
+
 const { createApp } = Vue
 
-createApp({
+const app = createApp({
     data() {
       return {
-        message: "funciona"
+        spells:[],
+        textSearch:""
       }
     },
     created(){
-        console.log(this.message);
+      this.getData(urlApi)
+        
     },
     methods: {
+        async getData(url){
+          const response = await fetch(urlApi) 
+          const data = await response.json()
+          const spellImg = data.map(element => {
+            return {
+              id: element.id,
+              name: element.name,
+              description: element.description,
+              img: img[element.name.toLowerCase()]
+            }
+          })
+          this.spells = spellImg
+          }
         
     },
     computed:{
+      searchSpells(){
+        return this.spells.filter(spell => spell.name.toLowerCase().includes(this.textSearch.toLowerCase())  )
+      }
 
     }
 
+
   }).mount('#app')
+
+  
