6173fca0477c22457f1a1b53cb54ffd052c8e050
LizPalma <lgpalma15@gmail.com>
2024-09-08 16:39:50 -0300

    compilacion en vue

--- src/js/stats.js
+++ src/js/stats.js
@@ -1,186 +1,202 @@
+
 import info from "../data/info.js"
 
-const { createApp } = Vue
+export default { //se tranfiere al html
+    template:
+        `
+        <main id="app" class="container-fluid d-flex justify-content-center align-items-center img-back p-3">
+        <!-- table stats -->
+        <div class="tables bg-light opacity-75 p-3">
+            <h5 class="text-center">Statistics</h5>
+            <div class="d-flex align-items-center justify-content-between col-6">
+                <canvas id="tableStudents" width="400" height="200"></canvas>
+                <canvas id="tableGender" width="400" height="200"></canvas>
+            </div>
+            <h5 class="text-center">Statistics Shop</h5>
+            <canvas id="tableProducts" width="400" height="200"></canvas>
+            
 
-createApp({
+            <!-- imagen gif -->
+            <img class="position-absolute col-2 bottom-0 end-0"
+                src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaXN5cm16cGJpajZxaDMxcnJ6OG4wcGhqMHplcmI5cjdodzRyb3JiZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/SsrQT2Au2Wn54fRxal/giphy.webp"
+                alt="">
+        </div>
+    
+    </main>
+         
+     `,
     data() {
         return {
             message: "funciona"
-        }
+        };
     },
     created() {
         console.log(this.message);
     },
-    methods: {
-        
+    mounted() {
+        this.fetchStudentData();
+        this.fetchProductData();
     },
-    computed: {
+    methods: {
+        fetchStudentData() {
+            fetch('https://hp-api.onrender.com/api/characters/students')
+                .then(response => response.json())
+                .then(data => {
+                    if (data && data.length) {
+                        // Process student data
+                        const houses = data.map(student => student.house);
+                        const countHouse = houses.reduce((acc, house) => {
+                            acc[house] = (acc[house] || 0) + 1;
+                            return acc;
+                        }, {});
 
-    }
+                        // Prepare data for Chart.js (houses)
+                        const tickets = Object.keys(countHouse);
+                        tickets[tickets.length - 1] = "Others";
+                        const values = Object.values(countHouse);
 
-}).mount('#app')
-//................table students 
+                        // Create House Chart.js instance
+                        this.createStudentsChart(tickets, values);
 
-// Obtener datos de la API
-fetch('https://hp-api.onrender.com/api/characters/students')
-    .then(response => response.json())
-    .then(data => {
-        // Procesar los datos para el gráfico
-        const names = data.map(student => student.name);
-        const houses = data.map(student => student.house);
+                        // Process gender data
+                        const filterGenders = data.map(student => student.gender);
+                        const countGender = filterGenders.reduce((acc, gender) => {
+                            acc[gender] = (acc[gender] || 0) + 1;
+                            return acc;
+                        }, {});
 
-        const filterGenders = data.map(student => student.gender);
-              // Contar el número de estudiantes por casa
-              const countGender = filterGenders.reduce((acc, gender) => {
-                acc[gender] = (acc[gender] || 0) + 1;
+                        // Prepare data for Chart.js (genders)
+                        const genderLabels = Object.keys(countGender);
+                        const genderValues = Object.values(countGender);
+
+                        // Create Gender Chart.js instance
+                        this.createGenderChart(genderLabels, genderValues);
+                    } else {
+                        alert("No student data found!");
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching student data:', error);
+                    alert("An error occurred while fetching student data.");
+                });
+        },
+        fetchProductData() {
+            // Process product data
+            const houses = info.products.map(product => product.house);
+            const countHouse = houses.reduce((acc, house) => {
+                acc[house] = (acc[house] || 0) + 1;
                 return acc;
             }, {});
-    
 
-        // Contar el número de estudiantes por casa
-        const countHouse = houses.reduce((acc, house) => {
-            acc[house] = (acc[house] || 0) + 1;
-            return acc;
-        }, {});
+            // Prepare data for Chart.js
+            const tickets = Object.keys(countHouse);
+            const values = Object.values(countHouse);
 
-        // Preparar los datos para Chart.js
-        const tickets = Object.keys(countHouse);
-        tickets[tickets.length - 1] = "Others"
-        const valuess = Object.values(countHouse);
-
-        const gender = Array.from(new Set(Object.values(filterGenders))) ;
-        const valuesGenders = Object.values(countGender);
-        console.log(gender);
-
-        // Crear el gráfico
-        let ctx = document.getElementById('tableStudents').getContext('2d');
-         new Chart(ctx, {
-            type: 'bar', // Tipo de gráfico
-            data: {
-                labels: tickets,
-                datasets: [{
-                    label: 'Students at Home',
-                    data: valuess,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 2)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 2)',
-                        'rgba(153, 102, 255, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
+            // Create Chart.js instance
+            this.createProductsChart(tickets, values);
+        },
+        createStudentsChart(labels, data) {
+            let ctx = document.getElementById('tableStudents').getContext('2d');
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Students at Home',
+                        data: data,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 2)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 2)',
+                            'rgba(153, 102, 255, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
                     }
                 }
-            }
-        });
-
-        let genderHtml = document.getElementById('tableGender').getContext('2d');
-        new Chart(genderHtml, {
-            type: 'bar', // Tipo de gráfico
-            data: {
-                labels: gender,
-                datasets: [{
-                    label: 'Genders',
-                    data: valuesGenders,
-                    backgroundColor: [
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        // 'rgba(75, 192, 192, 1)',
-                        // 'rgba(153, 102, 255, 1)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 2)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 2)',
-                        'rgba(153, 102, 255, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
+            });
+        },
+        createProductsChart(labels, data) {
+            let ctx = document.getElementById('tableProducts').getContext('2d');
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Products for Home',
+                        data: data,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.5)',
+                            'rgba(54, 162, 235, 0.5)',
+                            'rgba(255, 206, 86, 0.5)',
+                            'rgba(75, 192, 192, 0.5)',
+                            'rgba(153, 102, 255, 0.5)',
+                            'rgba(255, 159, 64, 0.5)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
                     }
                 }
-            }
-        });
-    })
-    .catch(error => console.error('Error al obtener los datos:', error));
-
-//.....................................table shop
-
-
-// Tus datos de productos
-const products = info.products
-
-// Agrega más productos si es necesario
-
-
-// Procesar los datos para el gráfico
-const houses1 = products.map(product => product.house);
-
-// Contar el número de productos por casa
-const countHouse1 = houses1.reduce((acc, house1) => {
-    acc[house1] = (acc[house1] || 0) + 1;
-    return acc;
-}, {});
-
-// Preparar los datos para Chart.js
-const tickets1 = Object.keys(countHouse1);
-const values1 = Object.values(countHouse1);
-
-// Crear el gráfico
-let printTable1 = document.getElementById('tableProducts').getContext('2d');
-let myTable1 = new Chart(printTable1, {
-    type: 'bar', // Tipo de gráfico
-    data: {
-        labels: tickets1,
-        datasets: [{
-            label: 'Products for Home',
-            data: values1,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.5)',
-                'rgba(54, 162, 235, 0.5)',
-                'rgba(255, 206, 86, 0.5)',
-                'rgba(75, 192, 192, 0.5)',
-                'rgba(153, 102, 255, 0.5)',
-                'rgba(255, 159, 64, 0.5)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+            });
+        },
+        createGenderChart(labels, data) {
+            let ctx = document.getElementById('tableGender').getContext('2d');
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Genders',
+                        data: data,
+                        backgroundColor: [
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(255, 206, 86, 1)',
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 2)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
         }
     }
-});
-
-//..............table circle
-
+};
