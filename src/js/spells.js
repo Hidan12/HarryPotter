eab5f0c82b94ccaaa6bfb21eafb8d8fd4bdb59fe
Danip25 <danielsambonic@hotmail.com>
2024-09-09 08:49:56 -0500

    style correction added modal

--- src/js/spells.js
+++ src/js/spells.js
@@ -13,7 +13,17 @@ const app = createApp({
         spells:[],
         textSearch:"",
         currentPage: 1,
-        itemsPerPage: 12, 
+        itemsPerPage: 12,
+        currentSpell: null,
+        quantityProductSaved: 0,
+        clickCarts: false,
+        ProductSaved: [],
+        bkProductSaved: [],
+        btnRemove: false,
+        deleteProduct: [],
+        totalPrice: 0,
+        BktotalPrice: 0,
+        btnBuy: false,
       }
     },
     created(){
@@ -49,8 +59,81 @@ const app = createApp({
         
           goToPage(page) {
             this.currentPage = page;
+          },
+
+          showSpell(spellId){
+            this.currentSpell = this.spells.find(spell => spell.id === spellId)
+          },
+          closeModal(){
+            this.currentSpell = null
+          },
+          closeBuy() {
+            this.btnBuy = !this.btnBuy
+            this.bkProductSaved = []
+            this.BktotalPrice = 0
+          },
+          buyProduct() {
+            this.clickCarts = !this.clickCarts
+            this.btnBuy = !this.btnBuy
+            this.bkProductSaved = [...this.ProductSaved]
+            this.ProductSaved = []
+            this.quantityProductSaved = 0
+            this.BktotalPrice = this.totalPrice
+            this.totalPrice = 0
+            localStorage.setItem("productsaved", JSON.stringify([]))
+          },
+          closeremuve() {
+            this.btnRemove = !this.btnRemove
+          },
+          deleteProducts(product = []) {
+            if (product.length > 0) {
+              this.deleteProduct = product
+            } else {
+              this.deleteProduct = this.ProductSaved
+            }
+            this.btnRemove = !this.btnRemove
+            this.clickCarts = !this.clickCarts
+          },
+          emptyProduct() {
+            if (this.btnRemove) {
+              this.btnRemove = false
+            }
+            this.ProductSaved = []
+            this.quantityProductSaved = 0
+            this.totalPrice = 0
+            localStorage.setItem("productsaved", JSON.stringify([]))
+      
+          },
+          removeProduct(card) {
+            if (this.btnRemove) {
+              this.btnRemove = false
+            }
+            const audio = new Audio("../../public/sound/transitional-swipe-3-211685.mp3")
+            audio.play()
+            let indexSaved = this.ProductSaved.findIndex(prod => prod.id === card.id);
+            if (indexSaved >= 0) {
+              this.totalPrice -= card.discount ? (card.price * (1 - (card.discount / 100))) : card.price
+              this.ProductSaved[indexSaved].add = false
+              this.ProductSaved.splice(indexSaved, 1)
+              localStorage.setItem("productsaved", JSON.stringify(this.ProductSaved))
+              this.quantityProductSaved = this.ProductSaved.length
+            }
+          },
+          clickCart() {
+            this.clickCarts = !this.clickCarts
+            if (this.clickCarts) {
+              const audio = new Audio("../../public/sound/wing-flap-heavy-prototype-36710.mp3")
+              audio.play()
+            } else {
+              const audio = new Audio("../../public/sound/poof-of-smoke-87381.mp3")
+              audio.play()
+            }
+          },
+          uploadinformation(data) {
+            this.quantityProductSaved = data.length
+            if (this.quantityProductSaved != 0) this.ProductSaved = data
           }
-        
+       
     },
     computed:{
       searchSpells(){
@@ -70,7 +153,14 @@ const app = createApp({
         const end = this.currentPage * this.itemsPerPage;
         return this.searchSpells.slice(start, end);
 
-    }
+    },
+    showSpells(){
+      return this.searchSpells.length > 0 ? this.searchSpells : this.spells
+    },
+    showSpellModal(){
+      console.log(this.currentSpell)
+      return this.currentSpell !== null
+    },
      }
 
 
